17c85df6c5d0824dbc064268570d048e53d25931
zhaohehuhu <luoyedeyi@163.com>
2025-08-29 20:39:44 +0800

    add catalog and queryId info
    Signed-off-by: zhaohehuhu <luoyedeyi@163.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ShowExecutor.java
@@ -912,7 +912,8 @@ public class ShowExecutor {
                             row.add(tConnectionInfo.getIsPending());
                             row.add(tConnectionInfo.getWarehouse());
                             row.add(tConnectionInfo.getCngroup());
-
+                            row.add(tConnectionInfo.getCatalog());
+                            row.add(tConnectionInfo.getQueryId());
                             rowSet.add(row);
                         }
                     } catch (TException e) {
