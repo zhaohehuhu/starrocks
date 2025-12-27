ac10e65dfc29ba83200010fa42f48e4fc2f70e11
He Zhao <luoyedeyi@163.com>
2025-12-28 02:41:03 +0800

    [BugFix] Fix query profile API to return a unified JSON format (#67077)
    Signed-off-by: zhaohehuhu <luoyedeyi@163.com>
    (cherry picked from commit 52b3a8a8a965023e62c105b293ae2fa4488bcca7)
    Signed-off-by: zhaohehuhu <luoyedeyi@163.com>

--- fe/fe-core/src/main/java/com/starrocks/http/rest/v2/ProfileActionV2.java
+++ fe/fe-core/src/main/java/com/starrocks/http/rest/v2/ProfileActionV2.java
@@ -68,13 +68,17 @@ public class ProfileActionV2 extends RestBaseAction {
             // If the query profile is not found in the local fe's ProfileManager,
             // we will query other frontend nodes to get the query profile.
             String queryPath = String.format(QUERY_PLAN_URI, queryId);
-            List<String> profileList = fetchResultFromOtherFrontendNodes(queryPath, authorization, HttpMethod.GET, false);
+            List<String> profileList =
+                    fetchResultFromOtherFrontendNodes(queryPath, authorization, HttpMethod.GET, false);
             for (String profile : profileList) {
                 if (profile != null) {
                     RestBaseResultV2<String> queryProfileResult = GsonUtils.GSON.fromJson(
                             profile,
                             new TypeToken<RestBaseResultV2<String>>() {
                             }.getType());
+                    if (queryProfileResult == null || queryProfileResult.getResult() == null) {
+                        continue;
+                    }
                     sendSuccessResponse(response, queryProfileResult.getResult(), request);
                     return;
                 }
