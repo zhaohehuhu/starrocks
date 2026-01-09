495303248c06953e39cd70050a84d41ac28bf22a
yan zhang <dirtysalt1987@gmail.com>
2026-01-09 15:38:56 +0800

    [Enhancement] support cast json to variant (#67648)
    Signed-off-by: yan zhang <dirtysalt1987@gmail.com>
    
    (cherry picked from commit a97d30adfd7242a75fa4c2e889738b170924547c)
    Signed-off-by: xuba <xuba@cisco.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/PrimitiveType.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/PrimitiveType.java
@@ -115,6 +115,7 @@ public enum PrimitiveType {
                     .add(BOOLEAN)
                     .addAll(NUMBER_TYPE_LIST)
                     .addAll(STRING_TYPE_LIST)
+                    .add(VARIANT)
                     .build();
     // TODO(mofei) support them
     public static final ImmutableList<PrimitiveType> JSON_UNCOMPATIBLE_TYPE =
